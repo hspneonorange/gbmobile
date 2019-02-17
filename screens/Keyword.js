aa1550994ff96db5186cf9c8dead3d5a53e816ef
Bryan Owen <bryandowen@hotmail.com>
2019-02-16 23:26:29 -0800

    Patched up /styles.js

--- screens/Keyword.js
+++ screens/Keyword.js
@@ -7,54 +7,12 @@ export default class Keyword extends Component{
       return (
         <ScrollView style={styles.scroll}>
             <View>
-              <TextInput
-                style={styles.textInput}
+              <TextInput style={styles.textInput}
                 placeholder="Enter search term"
                 onChangeText={(text) => this.setState({text})}
               />
             </View>
-            <ScrollView style={styles.searchResults}>
-            <View
-              style={styles.span}
-            />
-            <Button
-              onPress={() => {
-                Alert.alert("You've selected a product!");
-              }}
-              title="Product1"
-              color="#bcbcbc"
-            />
-            <View
-              style={styles.span}
-            />
-            <Button
-              onPress={() => {
-                Alert.alert("You've selected a product!");
-              }}
-              title="Product2"
-              color="#bcbcbc"
-            />
-            <View
-              style={styles.span}
-            />
-            <Button
-              onPress={() => {
-                Alert.alert("You've selected a product!");
-              }}
-              title="Product3"
-              color="#bcbcbc"
-            />
-            <View
-              style={styles.span}
-            />
-            </ScrollView>
-            <View
-              style={styles.span}
-            />
-            <View
-              style={styles.span}
-            />
-            <Button style={styles}
+            <Button style={styles.Button}
               onPress={() => {
               }}
               title="Search"
