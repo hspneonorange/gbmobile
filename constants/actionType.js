b5f6fe6e648df792720bd3d6de5144e98653a45a
Bryan Owen <bryandowen@hotmail.com>
2019-03-09 17:53:51 -0800

    Cart concatenated to salesQueue on checkout, userId collected on login, logout now works

---
+++ constants/actionType.js
@@ -0,0 +1,18 @@
+export default actionType = {
+    HANDLE_AUTHN: 'handle_authn',
+    LOGOUT: 'logout',
+    SEARCH_TEXT_CHANGED: 'search_text_changed',
+    RETURN_SEARCH_ITEMS: 'return_search_items',
+    RETURN_EVENTS: 'return_events',
+    USERNAME_TEXT_CHANGED: 'username_text_changed',
+    PASSWORD_TEXT_CHANGED: 'password_text_changed',
+    SET_EVENT: 'set_event',
+    UPDATE_USERID: 'update_userid',
+    ADD_TO_CART: 'add_to_cart',
+    DECREMENT_FROM_CART: 'decrement_from_cart',
+    REMOVE_FROM_CART: 'remove_from_cart',
+    EMPTY_CART: 'empty_cart',
+    CHECKOUT: 'checkout',
+    DISCOUNT_KEY_PRESS: 'discount_key_press',
+    CLEAR_STATE_ON_LOGOUT: 'clear_state_on_logout',
+};
